4d74e55db93323e7daaec637829688130391f718
TwikiRiki <twikiriki@gmail.com>
2026-02-08 19:34:51 +0100

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.jules.genealogy',
-  appName: 'Genealogy Tree Manager',
-  webDir: 'out/renderer'
-};
-
-export default config;
